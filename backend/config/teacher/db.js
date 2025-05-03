2c53a2fa5c84a06ea7b3a8d44d20fca2df492fb1
goodboykit <santiagogoodboykit@gmail.com>
2025-05-03 20:09:12 +0800

    commit pokingina

--- backend/config/teacher/db.js
+++ backend/config/teacher/db.js
@@ -1,13 +1,14 @@
+// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      dbName: 'LiterexiaDraft', 
+      dbName: 'teachers',     
     });
-    console.log('MongoDB Connected');
+    console.log('✅ MongoDB Connected');
   } catch (err) {
-    console.error('MongoDB connection failed:', err.message);
+    console.error('❌ MongoDB connection failed:', err.message);
     process.exit(1);
   }
 };
