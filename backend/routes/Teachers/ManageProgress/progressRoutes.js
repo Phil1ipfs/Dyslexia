c2b780549e982580b57577f8a5ba237d6dd81abb
goodboykit <santiagogoodboykit@gmail.com>
2025-09-15 11:48:52 +0800

    fix

--- backend/routes/Teachers/ManageProgress/progressRoutes.js
+++ backend/routes/Teachers/ManageProgress/progressRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const progressController = require('../../../controllers/Teachers/ManageProgress/progressController');
+const studentController = require('../../studentController');
 const { auth, authorize } = require('../../../middleware/auth');
 
 router.use(auth);
